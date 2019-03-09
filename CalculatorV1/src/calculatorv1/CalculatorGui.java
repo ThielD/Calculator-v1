e464f6b74dd13bef4eb92aa54abf5ea3e93ea88f
ThielD <32857619+ThielD@users.noreply.github.com>
2019-03-09 17:13:36 -0600

    Adding functionality to calculator.

--- CalculatorV1/src/calculatorv1/CalculatorGui.java
+++ CalculatorV1/src/calculatorv1/CalculatorGui.java
@@ -6,14 +6,26 @@ import javax.swing.JTextField;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CalculatorGui {
     
-    public void mainWindow(){
+    JTextField textField = new JTextField();
+    
+    double numberOne = 0;
+    double numberTwo = 0;
+    double answer = 0;
+    
+    enum Operator{
+        add, subtract, multiply, divide
+    }
+    Operator operator;
+    
+    public CalculatorGui(){
         
         double version = 1.0;
         JFrame mainFrame = new JFrame();
-        JTextField textField = new JTextField();
         JPanel buttonField = new JPanel();
         JButton buttonZero = new JButton("0");
         JButton buttonOne = new JButton("1");
@@ -55,7 +67,149 @@ public class CalculatorGui {
         buttonField.add(buttonThree);
         buttonField.add(addButton);
         
+        buttonZero.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               textField.setText(textField.getText() + "0");
+            }
+        });        
+        
+        buttonOne.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               textField.setText(textField.getText() + "1");
+            }
+        });
+        
+        buttonTwo.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               textField.setText(textField.getText() + "2");
+            }
+        });
+                
+        buttonThree.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               textField.setText(textField.getText() + "3");
+            }
+        });
+ 
+        buttonFour.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               textField.setText(textField.getText() + "4");
+            }
+        });                
+                
+         buttonFive.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               textField.setText(textField.getText() + "5");
+            }
+        });  
+         
+        buttonSix.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               textField.setText(textField.getText() + "6");
+            }
+        });  
+        
+        buttonSeven.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               textField.setText(textField.getText() + "7");
+            }
+        });  
+
+        buttonEight.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               textField.setText(textField.getText() + "8");
+            }
+        });  
+
+        buttonNine.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               textField.setText(textField.getText() + "9");
+            }
+        });
+
+        periodButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+               textField.setText(textField.getText() + ".");
+            }
+        });
+        
+        addButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                operator = Operator.add;
+                String value = textField.getText().toString();
+                numberOne = Double.parseDouble(value);
+                textField.setText("");
+            }
+        });
+        
+        subtractButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                operator = Operator.subtract;
+                String value = textField.getText().toString();
+                numberOne = Double.parseDouble(value);
+                textField.setText("");
+            }
+        });
+
+        multiplyButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                operator = Operator.multiply;
+                String value = textField.getText().toString();
+                numberOne = Double.parseDouble(value);
+                textField.setText("");
+            }
+        });
+
+        divideButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                operator = Operator.divide;
+                String value = textField.getText().toString();
+                numberOne = Double.parseDouble(value);
+                textField.setText("");
+            }
+        });
+
+        clearButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                numberOne = 0;
+                numberTwo = 0;
+                textField.setText("");
+            }
+        });
+
+        equalsButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String value = textField.getText().toString();
+                numberTwo = Double.parseDouble(value);
+                
+                switch(operator){
+                    case add:
+                        answer = numberOne + numberTwo;
+                    break;
+                        
+                    case subtract:
+                        answer = numberOne - numberTwo;
+                    break;
+                        
+                    case multiply:
+                        answer = numberOne * numberTwo;
+                    break;
+                        
+                    case divide:
+                        if (numberOne == 0.0 || numberTwo == 0.0){
+                            textField.setText("Cant divide by 0");
+                        } else {
+                            answer = numberOne / numberTwo;
+                        }
+                    break;
+                }
+                String answerString = String.valueOf(answer);
+                textField.setText(answerString);
+            }
+        });      
+   
         textField.setSize(200,150);
+        textField.setHorizontalAlignment(JTextField.RIGHT);
         
         buttonField.setLayout(new GridLayout(0, 4, 5, 5));
         buttonField.setSize(200, 300);
@@ -65,6 +219,7 @@ public class CalculatorGui {
         mainFrame.add(BorderLayout.SOUTH, equalsButton);
         
         mainFrame.setVisible(true);
+        
     }
     
 }
